1f432275ff29d8265bf4b179e9c41ad81293c66c
harveymannering <60523103+harveymannering@users.noreply.github.com>
2024-05-01 10:23:43 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='cmmd-pytorch',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='https://github.com/harveymannering/cmmd-pytorch',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['funniest'],
+      zip_safe=False)
